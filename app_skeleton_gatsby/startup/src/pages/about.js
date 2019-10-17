eb58815aa0a427645ab5c77f6c9fa0a844a07163
hoodielive <hoodielive@gmail.com>
2019-10-17 16:02:21 -0400

    add about page

---
+++ app_skeleton_gatsby/startup/src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => (
+	<div>
+		<h1>About Us</h1>
+		<p> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+				tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+				vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+				no sea takimata sanctus est Lorem ipsum dolor sit amet.
+		</p>
+	</div>
+)
+
+export default About
