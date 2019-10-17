05453fb80bac394760ea681f12b2c7bf8f74b8ac
hoodielive <hoodielive@gmail.com>
2019-10-17 16:12:13 -0400

    created team page

--- app_skeleton_gatsby/startup/src/pages/about.js
+++ app_skeleton_gatsby/startup/src/pages/about.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import Layout from '../components/layout'
 
 const About = () => (
-	<div>
-		<h1>About Us</h1>
-		<p> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-				tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
-				vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-				no sea takimata sanctus est Lorem ipsum dolor sit amet.
-		</p>
-	</div>
+	<Layout>
+		<div>
+			<h1>About Us</h1>
+			<p> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+					tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+					vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+					no sea takimata sanctus est Lorem ipsum dolor sit amet.
+			</p>
+		</div>
+	</Layout>
 )
 
 export default About
