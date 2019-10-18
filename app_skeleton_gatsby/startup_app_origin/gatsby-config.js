1ebc440f82f139871f75b1f895c01c393b2ebf46
hoodielive <hoodielive@gmail.com>
2019-10-17 20:37:05 -0400

    ..manipulation

--- app_skeleton_gatsby/startup_app_origin/gatsby-config.js
+++ app_skeleton_gatsby/startup_app_origin/gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Ibeji the Oracle`,
+    description: `The spirit of Gemini speaks to those who will listen...`,
     author: `@gatsbyjs`,
   },
   plugins: [
